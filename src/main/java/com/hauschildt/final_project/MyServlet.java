d840891cccc9e25d04b731360e15bf28eb80562e
Marc Hauschildt <marc.hauschildt@gmail.com>
2022-01-27 10:14:45 -0600

    stuff

---
+++ src/main/java/com/hauschildt/final_project/MyServlet.java
@@ -0,0 +1,19 @@
+package com.hauschildt.final_project;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "MyServlet", value = "/MyServlet")
+public class MyServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
